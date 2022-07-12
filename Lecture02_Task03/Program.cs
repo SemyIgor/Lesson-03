99b661c883b93576f822b2dc5d1c24d4a1932808
HenkSeed <adaner@inbox.ru>
2022-07-12 23:08:20 +0700

    Lecture02_Task03. Find array index of the given number

---
+++ Lecture02_Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Lecture02_Task03 (Task10). Find array index of the given number 
+
+int[] array = {1,12,31,4,18,15,16,17,18};
+int arrayLength = array.Length;
+
+int numberToFind = 18;
+
+int arrayIndex = 0;
+
+while(arrayIndex < arrayLength)
+{
+   if(array[arrayIndex] == numberToFind)
+   {
+      Console.WriteLine(arrayIndex);
+      break;
+   }
+   arrayIndex++;
+}
+
+// Console.WriteLine(arrayLength);
