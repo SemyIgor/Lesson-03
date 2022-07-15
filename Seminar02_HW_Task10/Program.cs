d883bdaccf90d00362dc82cdf846deff6c4f3ccb
HenkSeed <adaner@inbox.ru>
2022-07-15 23:48:54 +0700

    Seminar02_HW_Task10. Show second digit of the input 3-digit number

---
+++ Seminar02_HW_Task10/Program.cs
@@ -0,0 +1,39 @@
+﻿// Seminar02_HW_Task10
+// Show second digit of the input 3-digit number
+
+Console.Clear();
+Console.Write("Input 3-digit number: ");
+string? inputString = Console.ReadLine();
+
+if(inputString != null){
+   if(inputString.Length == 3)
+   {
+      //               Variant I
+      // Using char array
+      // =====================================================
+      char[] digits = inputString.ToCharArray();
+      Console.WriteLine($"The second digit is {digits[1]}");
+      // -----------------------------------------------------
+      //               End Variant I
+
+      //               Variant II
+      // Using string as array
+      // ========================================================
+      // Console.WriteLine($"The second digit is {inputString[1]}");
+      // --------------------------------------------------------
+      //              End Variant II
+
+      //                Variant III
+      // Using char array and ASCII-code of the digit
+      // ==============================================================
+      // char[] digits = inputString.ToCharArray();
+      // Console.WriteLine($"The second digit is {((int)digits[1] - 48)}");
+      // --------------------------------------------------------------
+      //                End Variant III
+   }
+   else
+   {
+      Console.WriteLine($"The number {inputString} is not 3-digit!");
+   }
+   
+}
