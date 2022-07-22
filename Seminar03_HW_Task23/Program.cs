9d710fa359852a1eb5410f4df6389ca4b0055a0f
HenkSeed <adaner@inbox.ru>
2022-07-23 02:42:49 +0700

    Seminar03_Task23. Input integer number and print this number and it's power 3.Table form

--- Seminar03_HW_Task23/Program.cs
+++ Seminar03_HW_Task23/Program.cs
@@ -7,46 +7,47 @@ if (numberStr != null)
 {
    int number = int.Parse(numberStr);
 
-   string[,] numberLine = new string[(number / 15) + 1, 15];
-   string[,] powerThreeLine = new string[number / 15 + 1, 15];
+   int numbersInLine = 14; // Columns in the table
+   string[,] numberLine = new string[(number / numbersInLine) + 1, numbersInLine];
+   string[,] powerThreeLine = new string[number / numbersInLine + 1, numbersInLine];
 
-   int n = number / 15;
+   int n = number / numbersInLine;
    int i = 0;
    string oneString = String.Empty;
    string onePower = String.Empty;
    while (i < n)
    {
-      oneString = "|";
-      onePower = "|";
-      for (int j = 0; j < 15; j++)
+      oneString = "|Число" + "\t|";
+      onePower = "|Куб" + "\t|";
+      for (int j = 0; j < numbersInLine; j++)
       {
-         numberLine[i, j] = (i * 15 + j + 1).ToString();
+         numberLine[i, j] = (i * numbersInLine + j + 1).ToString();
          powerThreeLine[i, j] = Math.Pow(Convert.ToInt32(numberLine[i, j]), 3).ToString();
          oneString = oneString + numberLine[i, j] + "\t|";
          onePower = onePower + powerThreeLine[i, j] + "\t|";
       }
 
-      Console.WriteLine(new string('-', 8 * 15));
+      Console.WriteLine(new string('-', 8 * (numbersInLine + 1)));
       Console.WriteLine(oneString);
-      Console.WriteLine(new string('-', 8 * 15));
+      Console.WriteLine(new string('-', 8 * (numbersInLine + 1)));
       Console.WriteLine(onePower);
-      Console.WriteLine(new string('-', 8 * 15));
+      Console.WriteLine(new string('-', 8 * (numbersInLine + 1)));
       i++;
    }
 
-   oneString = "|";
-   onePower = "|";
-   for (int j = 0; j < (number % 15); j++)
+   oneString = "|Число" + "\t|";
+   onePower = "|Куб" + "\t|";
+   for (int j = 0; j < (number % numbersInLine); j++)
    {
-      numberLine[i, j] = (i * 15 + j + 1).ToString();
+      numberLine[i, j] = (i * numbersInLine + j + 1).ToString();
       powerThreeLine[i, j] = Math.Pow(Convert.ToInt32(numberLine[i, j]), 3).ToString();
       oneString = oneString + numberLine[i, j] + "\t|";
       onePower = onePower + powerThreeLine[i, j] + "\t|";
    }
-   Console.WriteLine(new string('-', 8 * (number % 15)));
+   Console.WriteLine(new string('-', 8 * ((number % numbersInLine) + 1)));
    Console.WriteLine(oneString);
-   Console.WriteLine(new string('-', 8 * (number % 15)));
+   Console.WriteLine(new string('-', 8 * ((number % numbersInLine) + 1)));
    Console.WriteLine(onePower);
-   Console.WriteLine(new string('-', 8 * (number % 15)));
+   Console.WriteLine(new string('-', 8 * ((number % numbersInLine) + 1)));
    Console.WriteLine();
 }
