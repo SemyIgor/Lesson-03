51a8509974642fa7c7228c9a7e3a50b6f700413d
HenkSeed <adaner@inbox.ru>
2022-08-09 15:51:46 +0700

    Added second variant into Seminar05_Task31

--- Seminar05_Task31/Program.cs
+++ Seminar05_Task31/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Lesson-03_Seminar05_Task31. Generate 12-item array of numbers with values of [-9;9] 
 
 System.Random numberSintezator = new Random();
+int arrayLength = 12;
+
 
 void ProblemStatement() // Algorithms description
 {
@@ -8,6 +10,7 @@ void ProblemStatement() // Algorithms description
    // Generate 12-elements array of numbers with values of [-9;9]
 
    // Algorithms:
+   // VariantMy
    // 1. Declare sumPlus and sumMinus
    // 2. Declare 12-elements array
    // 2. Cycle (12 times):
@@ -16,33 +19,84 @@ void ProblemStatement() // Algorithms description
    //    - If number < 0 sumMinus+=sumMinus
    //    - If number == 0 does not matter and no influence on both sums, no need to check
    // 3. Print sumPlus, sumMinus;
+   // 
+   // VariantMain
+   // 1. Generate array
+   // 2. Cycle (Iterate array)
+   //    - If number > 0 sumPlus+=sumPlis
+   //    - If number < 0 sumMinus+=sumMinus
+   //    - If number == 0 does not matter and no influence on both sums, no need to check
+   // 3. Print sumPlus, sumMinus;
 }
 ProblemStatement(); // Just to take away warning message
 
-void VariantMain()
+void VariantMy()     // VariantI
+{
+   Console.WriteLine("Variant I");
+   int sumPlus = 0;     // Sum of positives
+   int sumMinus = 0;    // Sum of negatives
+   int[] array = new int[12];
+   Console.WriteLine("{0, 6}{1, 6}{2, 6}", "Array", "Pos", "Neg");      // Header of the output
+   for (int i = 0; i < 12; i++)                                         // Cycle
+   {
+      array[i] = numberSintezator.Next(-9, 9);  // Generate next array's element
+      if (array[i] < 0)
+      {
+         sumMinus += array[i];                  // Sum negatives
+         Console.Write("{0, 6}{1,6}{2,6}\n", array[i], "", array[i]);   // Print next line
+      }
+      else
+      {
+         sumPlus += array[i];                   // Sum positives
+         Console.Write("{0, 6}{1,6}{2,6}\n", array[i], array[i], "");   // Print next line
+      }
+   }
+   Console.Write("{0, 6}{1,6}{2,6}\n", "Итого", sumPlus, sumMinus); // Print result line
+   Console.WriteLine();
+}
+
+void VariantMain()   // VariantII
+{
+   Console.WriteLine("Variant I");
+   IterateArray(GenerateArray(arrayLength));
+   Console.WriteLine();
+}
+
+int[] GenerateArray(int arrayLength)   // Generate array with length received 
+{
+   int[] array = new int[arrayLength];
+   for (int i = 0; i < 12; i++)
+   {
+      array[i] = numberSintezator.Next(-9, 9); // Generates next random element
+   }
+   return array;
+}
+
+void IterateArray(int[] array)
 {
    int sumPlus = 0;
    int sumMinus = 0;
-   int[] array = new int[12];
-   Console.WriteLine("{0, 6}{1, 6}{2, 6}", "Array", "Pos", "Neg");
-   for (int i = 0; i < 12; i++)
+   int arrayLength = array.Length;
+
+   Console.WriteLine("{0, 6}{1, 6}{2, 6}", "Array", "Pos", "Neg");   // Header of the output
+
+   for (int i = 0; i < arrayLength; i++) // Iterate received array
    {
-      array[i] = numberSintezator.Next(-9, 9);
       if (array[i] < 0)
       {
-         sumMinus += array[i];
+         sumMinus += array[i];      // Sum negatives
          Console.Write("{0, 6}{1,6}{2,6}\n", array[i], "", array[i]);
       }
       else
       {
-         sumPlus += array[i];
+         sumPlus += array[i];       // Sum positives
          Console.Write("{0, 6}{1,6}{2,6}\n", array[i], array[i], "");
       }
-
-      // Console.WriteLine(sumMinus, sumPlus, array[i]);
    }
-   Console.Write("{0, 6}{1,6}{2,6}\n", "Итого", sumPlus, sumMinus);
-
+   Console.Write("{0, 6}{1,6}{2,6}\n", "Итого", sumPlus, sumMinus);  // Print result line
 }
 
+
 VariantMain();
+
+VariantMy();
