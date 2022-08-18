280f3fe11f5941d393b243df563982d0f0845301
HenkSeed <adaner@inbox.ru>
2022-08-18 19:43:46 +0700

    Added some comments and changed rounding from 2 to 4 signs

--- Seminar07_HW_Task47/Program.cs
+++ Seminar07_HW_Task47/Program.cs
@@ -1,12 +1,10 @@
-﻿// Generate a two-dimensional array of the required sizes with real numbers
+﻿// Lesson-03_Seminar07_Task47. Generate a two-dimensional array of the required sizes with real numbers
 // * Colorize every digit of it's elements
 
 void GenerateFloatTwoDimArray()
 {
-   // int rows = 20; // Height of the matrix
-   // int columns = 15; // Width of the matrix
-   double minValue = 0; // Min Value of elements
-   double maxValue = 21; // Max Value of elements
+   double minValue = 5; // Min Value of elements
+   double maxValue = 25; // Max Value of elements
 
    Console.Clear();
 
@@ -14,11 +12,10 @@ void GenerateFloatTwoDimArray()
    int rows = InputStringToInteger(rowInvitationMessage); // Input number of rows of the array
 
    string ColInvitationMessage = "Введите количество столбцов таблицы:\n"; // Invitation message
-   int columns = InputStringToInteger(ColInvitationMessage); // IInput number of columns of the array
+   int columns = InputStringToInteger(ColInvitationMessage); // Input number of columns of the array
 
-   double[,] generateTwoDimArray = DoubleGenerateTwoDimArray(rows, columns, minValue, maxValue);
+   double[,] generateTwoDimArray = DoubleGenerateTwoDimArray(rows, columns, minValue, maxValue); // Generate two-dim array
    PrintTwoDimArray(generateTwoDimArray);
-
 }
 
 double[,] DoubleGenerateTwoDimArray(int row, int col, double minValue, double maxValue) // Generate two-dimentional array
@@ -29,24 +26,23 @@ double[,] DoubleGenerateTwoDimArray(int row, int col, double minValue, double ma
    {
       for (int j = 0; j < twoDimArray.GetLength(1); j++) // Columns
       {
-         // twoDimArray[i, j] = randomSintezator.Next(min, max);
-
-         twoDimArray[i, j] = randomSintezator.NextDouble() *
-         (maxValue + 1 - minValue); // Generates next random element
-         twoDimArray[i, j] = Math.Round(twoDimArray[i, j], 2); // Round value to two digits
+         twoDimArray[i, j] =
+         minValue +
+         randomSintezator.NextDouble() *
+         (maxValue - minValue); // Generates next random element in range from minValue to maxValue
+         twoDimArray[i, j] = Math.Round(twoDimArray[i, j], 4); // Round value to four digits
       }
    }
-   return twoDimArray;
+   return twoDimArray; // return array
 }
 
 void PrintTwoDimArray(double[,] twoDimArray) // Print two-dimentional array
 {
-   // Console.ForegroundColor = ConsoleColor.Yellow; // Changes color of the text in console
    for (int i = 0; i < twoDimArray.GetLength(0); i++) // Rows
    {
       for (int j = 0; j < twoDimArray.GetLength(1); j++) // Columns
       {
-         Console.Write("{0, 7}", twoDimArray[i, j]);
+         Console.Write("{0, 9}", twoDimArray[i, j]); // Print the next element
       }
       Console.WriteLine(); // Empty string to separate lines
    }
@@ -59,4 +55,4 @@ int InputStringToInteger(string message) // Input string and convert it to integ
    return integerNumber; // Return inputed number 
 }
 
-GenerateFloatTwoDimArray();
+GenerateFloatTwoDimArray(); // Starts main method
