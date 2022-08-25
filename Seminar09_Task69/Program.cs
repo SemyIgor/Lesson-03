2bb85580a574268fdbcd862fba5b5f4e7b7bfaa3
HenkSeed <adaner@inbox.ru>
2022-08-25 23:35:29 +0700

    Changed some comments

--- Seminar09_Task69/Program.cs
+++ Seminar09_Task69/Program.cs
@@ -8,16 +8,24 @@ void RaiseAToPowerB()
 
    try
    {
-      int baseNumber;
-      int exponentNumber;
+      int baseNumber; // Base of the number
+      int exponentNumber; // Exponent of the number
 
       // Enter number and it's power
       string enterTwoNumbersStr = InputString("Укажите число и его степень через запятую ");
       TakeNumbers(enterTwoNumbersStr, ",", out baseNumber, out exponentNumber); // Extract from the string two numbers
 
       Console.WriteLine("Основание степени = {0}; Показатель степени = {1}", baseNumber, exponentNumber); // Prints base and exponent
-      int result = RecursPowerNum(baseNumber, exponentNumber);
-      Console.WriteLine("Результат = {0}", result);
+
+      // Saves entered numbers for the second variant
+      int saveBase = baseNumber;
+      int saveExp = exponentNumber;
+
+      int resultK = RecursPowerNum(baseNumber, exponentNumber);
+      Console.WriteLine("Результат метода Кирилла = {0}", resultK);
+
+      int resultA = RecurseExt(saveBase, saveExp);
+      Console.WriteLine("Результат метода Андрея = {0}", resultA);
    }
    catch
    {
@@ -44,6 +52,20 @@ int RecursPowerNum(int baseA, int expB) // Kirill's solution
    // The end of plunging if the power is 1
    if (expB == 1) return baseA;
    return baseA * RecursPowerNum(baseA, --expB); // Recursive multiplying baseA expB times
+}
+
+int RecurseExt(int baseA, int expB) // Andrey's solution
+{
+   if (expB == 2) return baseA * baseA; // In case the number of last multiplication is even
+   if (expB == 1) return baseA; // In case the number of last multiplication is odd
+   if (expB % 2 == 0) // In case the number of multiplications is even
+   {
+      return RecurseExt(baseA, (expB / 2)) * RecurseExt(baseA, (expB / 2));
+   }
+   else // In case the number of multiplications is odd
+   {
+      return RecurseExt(baseA, (expB / 2)) * RecurseExt(baseA, (expB / 2) + 1);
+   }
 }
 
 RaiseAToPowerB();
